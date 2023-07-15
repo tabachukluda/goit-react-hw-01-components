c2b11d898cf67a1f2edcc25cea617c8c86c3ec1b
tabachukluda <121580942+tabachukluda@users.noreply.github.com>
2023-07-15 10:08:00 +0200

    v.2

--- src/index.js
+++ src/index.js
@@ -3,8 +3,34 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+
+import { Profile } from 'components/profile/Profile';
+import user from './components/profile/user.json';
+
+import { Statistics } from 'components/statistics/Statistics';
+import data from './components/statistics/data.json';
+
+import { FriendList } from 'components/friend-list/FriendList';
+import friends from './components/friend-list/friends.json';
+
+import { TransactionHistory } from 'components/transactions-history/TransactionHistory';
+import transactions from './components/transactions-history/transactions.json';
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+    />
+    <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />
+    </App>
+    </React.StrictMode>
 );
